51aa49deb8ac13d9c6e9a194a6337c6ae403ee35
Oveth29 <ovethrobleto56@gmail.com>
2024-10-18 18:07:03 -0600

    Actualizacion

--- braille_tech/macos/Flutter/GeneratedPluginRegistrant.swift
+++ braille_tech/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import speech_to_text
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
